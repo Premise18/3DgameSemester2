45b0780f0cdd3ff546fbf7f133ec59bb7b7a8cef
Tameeah <tameeah.padayachy@gmail.com>
2025-08-12 11:01:55 +0200

    Pick Up Mechanic
    Added the pick up mechanic - the red sphere, new script, new mechanic.

--- Assets/scripts/FPController.cs
+++ Assets/scripts/FPController.cs
@@ -23,6 +23,11 @@ public class FPController : MonoBehaviour
     public float crouchSpeed = 2.5f;
     private float originalMoveSpeed;
 
+    [Header("Pickup Settings")]
+    public float pickupRange = 3f;
+    public Transform holdPoint;
+    private PickUpObject heldObject;
+
     private CharacterController controller;
     private Vector2 moveInput;
     private Vector2 lookInput;
@@ -41,6 +46,11 @@ public class FPController : MonoBehaviour
     {
         HandleMovement();
         HandleLook();
+
+        if (heldObject != null)
+        {
+            heldObject.MoveToHoldPoint(holdPoint.position);
+        }
     }
     public void OnMovement(InputAction.CallbackContext context)
     {
@@ -96,7 +106,31 @@ public class FPController : MonoBehaviour
         }
     }
 
-    
+    public void OnPickUp(InputAction.CallbackContext context)
+    {
+        if (!context.performed) return;
+
+        if (heldObject == null)
+        {
+            Ray ray = new Ray(cameraTransform.position, cameraTransform.forward);
+            if (Physics.Raycast(ray, out RaycastHit hit, pickupRange))
+            {
+                PickUpObject pickUp = hit.collider.GetComponent<PickUpObject>();
+                if (pickUp != null)
+                {
+                    pickUp.PickUp(holdPoint);
+                    heldObject = pickUp;
+                }
+            }
+        }
+        else
+        {
+            heldObject.Drop();
+            heldObject = null;
+        }
+    }
+
+
     public void HandleMovement()
     {
         Vector3 move = transform.right * moveInput.x + transform.forward *
